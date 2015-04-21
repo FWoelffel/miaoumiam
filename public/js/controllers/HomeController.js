a3f5fe7398feb88fbb85db3a455259595b476dd3
Frédéric Woelffel <woelffel.f@gmail.com>
2015-04-21 18:54:48 +0200

    Updated frontend controller attributes. It now stores a local copy of the server config.

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -1,24 +1,51 @@
 var HomeController = MiaouMiam.controller('HomeController', function($scope, $http) {
 
-    $scope.feeding = false;
-    $scope.scheduling = false;
+    /**
+     * Functions
+     */
 
-    var d = new Date();
-    $scope.date = {
-        hours: d.getHours(),
-        minutes: d.getMinutes(),
-        monday: false,
-        tuesday: false,
-        wednesday: false,
-        thursday: false,
-        satursday: false,
-        sunday: false
+    $scope.loadCfg = function() {
+        $scope.loadingCfg = true;
+        $http.get('/config')
+            .then(function onSuccess(response) {
+                if(response.status === 200) {
+                    $scope.config = response.data;
+                }
+                else {
+                    console.log('Error : ' + response.data);
+                }
+            })
+            .catch(function onError(err){
+                console.log('Error : ' + response.data);
+            })
+            .finally(function eitherWay(){
+                $scope.loadingCfg = false;
+            });
+    }
+
+    $scope.saveCfg = function() {
+        $scope.savingCfg = true;
+        $http.post('/config', {config: $scope.config})
+            .then(function onSuccess(response) {
+                if(response.status === 200) {
+                    $scope.config = response.data;
+                }
+                else {
+                    console.log('Error : ' + response.data);
+                }
+            })
+            .catch(function onError(err){
+                console.log('Error : ' + response.data);
+            })
+            .finally(function eitherWay(){
+                $scope.savingCfg = false;
+            });
     }
 
     $scope.feed = function() {
         console.log('Feeding in progress');
         $scope.feeding = true;
-        $http.post('/feed', {times: $scope.slider.val})
+        $http.post('/feed', {times: $scope.config.quantity})
             .then(function onSuccess(response){
                 if(response.status === 200) {
                     console.log('Feeding terminated');
@@ -40,23 +67,38 @@ var HomeController = MiaouMiam.controller('HomeController', function($scope, $ht
         $scope.date.minutes = $scope.time.getMinutes();
     }
 
-    $scope.schedule = function() {
-        $scope.scheduling = true;
-        console.log('Scheduling');
-        $http.post('/config/schedule', {date: $scope.date})
-            .then(function onSuccess(response) {
-                if (response.status === 200) {
-                    console.log('Scheduled');
-                }
-                else {
-                    console.log('Error : ' + response.data);
-                }
-            })
-            .catch(function onError(err) {
-                console.log('Error : ' + response.data);
-            })
-            .finally(function eitherWay() {
-                $scope.scheduling = false;
-            });
+    $scope.addSchedule = function() {
+        $scope.config.schedule.push($scope.date);
+        $scope.saveCfg();
+    }
+
+    $scope.removeSchedule = function(idx) {
+        $scope.config.schedule.splice(idx, 1); // TODO test
+        $scope.saveCfg();
+    }
+
+    /**
+     * Initialization
+     */
+
+    {
+        $scope.feeding = false;
+        $scope.loadingCfg = false;
+        $scope.savingCfg = false;
+
+        // Init default date
+        var d = new Date();
+        $scope.date = {
+            hours: d.getHours(),
+            minutes: d.getMinutes(),
+            monday: false,
+            tuesday: false,
+            wednesday: false,
+            thursday: false,
+            satursday: false,
+            sunday: false
+        };
+
+        $scope.loadCfg();
     }
 });
--- routes/config.js
+++ routes/config.js
@@ -2,6 +2,19 @@ var express = require('express');
 var router = express.Router();
 var configReader = require('../services/config');
 
+router.get('/', function(req, res, next) {
+    configReader.read(function(config) {
+        res.json(config);
+    });
+});
+
+router.post('/', function(req, res, next) {
+    var config = req.body.config;
+    configReader.write(config, function() {
+        res.sendStatus(200);
+    })
+});
+
 router.post('/schedule', function(req, res, next) {
     var date = req.body.date;
     configReader.read(function(config) {
